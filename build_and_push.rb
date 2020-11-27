820ab60914db055d60d2bcf412e0d96253e3c094
jasl <jasl9187@hotmail.com>
2020-11-28 01:43:26 +0800

    update

--- build_and_push.rb
+++ build_and_push.rb
@@ -1,18 +1,19 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-GIT_TAG = "poc3-1.0"
+BUILD_ONLY = false
+GIT_TAG = "poc3-1.0.3"
 
 NODE_DOCKER_REPO = "phala-poc3-node"
-NODE_DOCKER_TAG = "1.0.0"
+NODE_DOCKER_TAG = "poc3-1.0.3"
 NODE_GIT_TAG = GIT_TAG
 
 PHOST_DOCKER_REPO = "phala-poc3-phost"
-PHOST_DOCKER_TAG = "1.0.0"
+PHOST_DOCKER_TAG = "poc3-1.0.3"
 PHOST_GIT_TAG = GIT_TAG
 
 PRUNTIME_DOCKER_REPO = "phala-poc3-pruntime"
-PRUNTIME_DOCKER_TAG = "1.0.0"
+PRUNTIME_DOCKER_TAG = "poc3-1.0.3"
 
 SGX_DETECT_DOCKER_REPO = "phala-sgx_detect"
 
@@ -37,52 +38,96 @@ def run(cmd)
   end
 end
 
-# Build && Push Phala-PRuntime
+# # Build SGX_Detect
+# REGISTRIES.each do |registry|
+#   [
+#     "docker build -f sgx_detect.Dockerfile -t #{registry}/#{SGX_DETECT_DOCKER_REPO} ."
+#   ].each do |cmd|
+#     puts cmd
+#     run cmd
+#   end
+# end
+
+# unless BUILD_ONLY
+#   # Push SGX_Detect
+#   REGISTRIES.each do |registry|
+#     [
+#       "docker push #{registry}/#{SGX_DETECT_DOCKER_REPO}"
+#     ].each do |cmd|
+#       puts cmd
+#       run cmd
+#     end
+#   end
+# end
+
+# Build Phala-PRuntime
 REGISTRIES.each do |registry|
   [
     "docker build -f prebuilt-pruntime.Dockerfile -t #{registry}/#{PRUNTIME_DOCKER_REPO}:#{PRUNTIME_DOCKER_TAG} .",
-    "docker build -f prebuilt-pruntime.Dockerfile -t #{registry}/#{PRUNTIME_DOCKER_REPO} .",
-    "docker push #{registry}/#{PRUNTIME_DOCKER_REPO}:#{PRUNTIME_DOCKER_TAG}",
-    "docker push #{registry}/#{PRUNTIME_DOCKER_REPO}"
+    "docker build -f prebuilt-pruntime.Dockerfile -t #{registry}/#{PRUNTIME_DOCKER_REPO} ."
   ].each do |cmd|
     puts cmd
     run cmd
   end
 end
 
-# Build && Push SGX_Detect
-REGISTRIES.each do |registry|
-  [
-    "docker build -f sgx_detect.Dockerfile -t #{registry}/#{SGX_DETECT_DOCKER_REPO} .",
-    "docker push #{registry}/#{SGX_DETECT_DOCKER_REPO}"
-  ].each do |cmd|
-    puts cmd
-    run cmd
+unless BUILD_ONLY
+  # Push Phala-PRuntime
+  REGISTRIES.each do |registry|
+    [
+      "docker push #{registry}/#{PRUNTIME_DOCKER_REPO}:#{PRUNTIME_DOCKER_TAG}",
+      "docker push #{registry}/#{PRUNTIME_DOCKER_REPO}"
+    ].each do |cmd|
+      puts cmd
+      run cmd
+    end
   end
 end
 
-# Build && Push Phala-Node
+# Build Phala-Node
 REGISTRIES.each do |registry|
   [
     "docker build --build-arg PHALA_GIT_TAG=#{NODE_GIT_TAG} -f node.Dockerfile -t #{registry}/#{NODE_DOCKER_REPO}:#{NODE_DOCKER_TAG} .",
-    "docker build --build-arg PHALA_GIT_TAG=#{NODE_GIT_TAG} -f node.Dockerfile -t #{registry}/#{NODE_DOCKER_REPO} .",
-    "docker push #{registry}/#{NODE_DOCKER_REPO}:#{NODE_DOCKER_TAG}",
-    "docker push #{registry}/#{NODE_DOCKER_REPO}"
+    "docker build --build-arg PHALA_GIT_TAG=#{NODE_GIT_TAG} -f node.Dockerfile -t #{registry}/#{NODE_DOCKER_REPO} ."
   ].each do |cmd|
     puts cmd
     run cmd
   end
 end
 
-# Build && Push Phala-PHost
+unless BUILD_ONLY
+  # Push Phala-Node
+  REGISTRIES.each do |registry|
+    [
+      "docker push #{registry}/#{NODE_DOCKER_REPO}:#{NODE_DOCKER_TAG}",
+      "docker push #{registry}/#{NODE_DOCKER_REPO}"
+    ].each do |cmd|
+      puts cmd
+      run cmd
+    end
+  end
+end
+
+# Build Phala-PHost
 REGISTRIES.each do |registry|
   [
     "docker build --build-arg PHALA_GIT_TAG=#{PHOST_GIT_TAG} -f phost.Dockerfile -t #{registry}/#{PHOST_DOCKER_REPO}:#{PHOST_DOCKER_TAG} .",
-    "docker build --build-arg PHALA_GIT_TAG=#{PHOST_GIT_TAG} -f phost.Dockerfile -t #{registry}/#{PHOST_DOCKER_REPO} .",
-    "docker push #{registry}/#{PHOST_DOCKER_REPO}:#{PHOST_DOCKER_TAG}",
-    "docker push #{registry}/#{PHOST_DOCKER_REPO}"
+    "docker build --build-arg PHALA_GIT_TAG=#{PHOST_GIT_TAG} -f phost.Dockerfile -t #{registry}/#{PHOST_DOCKER_REPO} ."
   ].each do |cmd|
     puts cmd
     run cmd
+  end
+end
+
+unless BUILD_ONLY
+  # Push Phala-PHost
+  REGISTRIES.each do |registry|
+    [
+      "docker push #{registry}/#{PHOST_DOCKER_REPO}:#{PHOST_DOCKER_TAG}",
+      "docker push #{registry}/#{PHOST_DOCKER_REPO}"
+    ].each do |cmd|
+      puts cmd
+      run cmd
+    end
   end
 end
