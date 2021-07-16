b278163c68befe483298d2ca6ebdad9d5ddedc8a
h4x3rotab <h4x3rotab@gmail.com>
2021-07-17 00:01:51 +0800

    User friendly promote when starting pruntime

--- dockerfile.d/start_pruntime.sh
+++ dockerfile.d/start_pruntime.sh
@@ -19,10 +19,13 @@ else
   /bin/chmod 0750 /var/opt/aesmd/
 
   LD_LIBRARY_PATH=/opt/intel/sgx-aesm-service/aesm /opt/intel/sgx-aesm-service/aesm/aesm_service &
+  
+  echo "pRuntime starting... It may take up to 2 minutes."
 
   SLEEP_BEFORE_START=${SLEEP_BEFORE_START:-"0"}
   if [ ! "$SLEEP_BEFORE_START" == "0" ]
   then
+    echo "Waiting for device. Sleep ${SLEEP_BEFORE_START}s"
     echo "Sleep ${SLEEP_BEFORE_START}s"
 
     sleep "$SLEEP_BEFORE_START"
