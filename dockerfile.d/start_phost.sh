b1a1bd59b1d16c4b93879691c4193867905f31bc
h4x3rotab <h4x3rotab@gmail.com>
2021-07-16 23:57:40 +0800

    User friendly welcome message for pRuntime startup

--- dockerfile.d/start_phost.sh
+++ dockerfile.d/start_phost.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+echo "pRuntime starting... It may take up to 2 minutes."
+
 SLEEP_BEFORE_START=${SLEEP_BEFORE_START:-"0"}
 if [ ! "$SLEEP_BEFORE_START" == "0" ]
 then
-  echo "Sleep ${SLEEP_BEFORE_START}s"
+  echo "Waiting for device. Sleep ${SLEEP_BEFORE_START}s"
 
   sleep "$SLEEP_BEFORE_START"
 fi
